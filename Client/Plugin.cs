54f2b09ec33e76af9ef284eaf04e77b503f3de91
ShaneeexD <31047364+ShaneeexD@users.noreply.github.com>
2026-06-26 15:14:17 +0100

    Bump version to 2.0.2

--- Client/Plugin.cs
+++ Client/Plugin.cs
@@ -6,7 +6,7 @@ using TraderGen.Client.Patches;
 
 namespace TraderGen.Client
 {
-    [BepInPlugin("com.tradergen.client", "TraderGen Client", "2.0.1")]
+    [BepInPlugin("com.tradergen.client", "TraderGen Client", "2.0.2")]
     public class Plugin : BaseUnityPlugin
     {
         internal static ConfigEntry<bool> EnableExportButton { get; private set; }
--- Server/TraderGenPlugin.cs
+++ Server/TraderGenPlugin.cs
@@ -21,7 +21,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "TraderGen";
     public override string Author { get; init; } = "Serenity";
     public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("2.0.1");
+    public override SemanticVersioning.Version Version { get; init; } = new("2.0.2");
     public override SemanticVersioning.Range SptVersion { get; init; } = new("4.0.13");
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = new()
